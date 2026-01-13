8e999ba7613e4f4a386025ad06eee2e6055cc7ee
PrinDDI <diletta.abbonato@unimib.it>
2026-01-13 01:27:02 +0100

    Configure deployment workflow with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/diversity-citation-networks/',
   plugins: [react()],
   server: {
     port: 3000,
